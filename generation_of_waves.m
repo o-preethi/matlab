5351b99634bf5cb2381285c93b410b5708ee831b
o-preethi <preethio.be27@uceou.edu>
2025-05-08 21:22:15 +0530

    Add files via upload

---
+++ generation_of_waves.m
@@ -0,0 +1,42 @@
+fs = 500;          % Sampling frequency
+t = 0:1/fs:1;       % Time 
+f = 5;              % Frequency 
+A = 1.2;            % Amplitude 
+
+% Generation of waveforms
+sinewave = A * sin(2 * pi * f * t);               
+squarewave = A * square(2 * pi * f * t);           
+trianglewave = A * sawtooth(2 * pi * f * t, 0.5);  
+sawtoothwave = A * sawtooth(2 * pi * f * t);       
+
+
+% Sine Wave
+subplot(411);
+plot(t, sinewave);
+title('Sine Wave');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+% Square Wave
+subplot(412);
+plot(t, squarewave);
+title('Square Wave');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+
+% Triangle Wave
+subplot(413);
+plot(t, trianglewave);
+title('Triangle Wave');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+% Sawtooth Wave
+subplot(414);
+plot(t, sawtoothwave);
+title('Sawtooth Wave');
+xlabel('Time (s)');
+ylabel('Amplitude');
+
+
