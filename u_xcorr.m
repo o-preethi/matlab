015aede106beeff9587f2204f9bd07dde6fa0275
o-preethi <preethio.be27@uceou.edu>
2025-05-08 21:43:32 +0530

    u_xcorr.m

--- u_xcorr.m
+++ u_xcorr.m
@@ -1,6 +1,6 @@
 %cross correlation
 function u_xcorr(x,h,N)
-if nargin==2||nargin==3
+if nargin==2||nargin==3 %performs cross correlation based on the two input signals provided
     n=length(x);
     m=length(h);
     op_len=m+n-1;
@@ -11,7 +11,7 @@ for i=1:op_len
         hrm=[zeros(1,i-1) hm(1:op_len-i+1)];
         y(i)=(xm*hrm');
  end 
-if nargin==3
+if nargin==3 %if operation length is provided
     if N+1==n
         y=[0 y 0];
     elseif N+1<n
